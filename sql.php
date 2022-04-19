cc5b3f684d5978aebe23266f918b8ea89aa9744f
黄如庆 <104032253+daytoosmile@users.noreply.github.com>
2022-04-20 05:19:07 +0800

    Delete sql.php

--- sql.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "47.112.122.92";
-$username = "smile";
-$password = "7yX6JfNr5f2XcfhR";
-$dbname = "test";
-$conn = new mysqli($servername, $username, $password, $dbname);
-mysqli_query($conn,"set names utf8");
-?>
